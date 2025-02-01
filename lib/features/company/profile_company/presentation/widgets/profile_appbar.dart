f787184ec2f6847ade724bfedbc8e1939bf9b750
yuossefghareb <yuossefghareb27@gmail.com>
2025-02-01 12:59:28 +0200

    merginngggggggggg

--- lib/features/company/profile_company/presentation/widgets/profile_appbar.dart
+++ lib/features/company/profile_company/presentation/widgets/profile_appbar.dart
@@ -4,11 +4,13 @@ import 'package:flutter_svg/svg.dart';
 import 'package:tradof/core/helpers/spacing.dart';
 import 'package:tradof/core/theming/app_colors.dart';
 import 'package:tradof/core/theming/app_style.dart';
-
-import 'user_image_and_info.dart';
+import 'package:tradof/features/company/profile_company/data/model/company_model.dart';
+import 'package:tradof/features/company/profile_company/presentation/widgets/user_image_and_info.dart';
 
 class ProfileAppbar extends StatelessWidget {
   const ProfileAppbar({super.key, required this.companyModel});
+
+  final CompanyModel companyModel;
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -31,12 +33,11 @@ class ProfileAppbar extends StatelessWidget {
                 SvgPicture.asset('assets/images/user.svg')
               ],
             ),
-          ),
-          verticalSpace(20),
-          UserImageAndInfo()
-
-        ],
-        
+            verticalSpace(10),
+           UserImageAndInfo(companyModel: companyModel,),
+            verticalSpace(12),
+          ],
+        ),
       ),
     );
   }
